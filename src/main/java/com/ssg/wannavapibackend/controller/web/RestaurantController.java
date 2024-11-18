91ae462270af75eae86409b2443eaf9e51c53b83
nam3280 <nam3280@gmail.com>
2024-11-18 18:53:22 +0900

    test

---
+++ src/main/java/com/ssg/wannavapibackend/controller/web/RestaurantController.java
@@ -0,0 +1,20 @@
+package com.ssg.wannavapibackend.controller.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Log4j2
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/restaurant")
+public class RestaurantController {
+
+    @GetMapping()
+    public String test1(Model model) {
+        model.addAttribute("restaurantId",1);
+        return "restaurant/reservation2";
+    }
+}
